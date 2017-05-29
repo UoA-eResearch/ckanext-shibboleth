3445be16eb0846a1691ab3a193bf6d840528dd97
Prashant Gupta <prashantgupta.nz@gmail.com>
2017-05-29 16:28:12 +1200

    Update controller.py

--- ckanext/shibboleth/controller.py
+++ ckanext/shibboleth/controller.py
@@ -7,7 +7,8 @@ import logging
 from pylons.i18n import _
 
 import ckan.controllers.user as user
-import ckan.lib.base as base
+#import ckantoolkit.base as base
+import ckan.plugins.toolkit as toolkit
 
 log = logging.getLogger(__name__)
 
@@ -15,12 +16,12 @@ log = logging.getLogger(__name__)
 class ShibbolethController(user.UserController):
 
     def shiblogin(self):
-        if base.c.userobj is not None:
+        if toolkit.c.userobj is not None:
             log.info("Repoze.who Shibboleth controller received userobj %r " % base.c.userobj)
-            return base.h.redirect_to(controller='user',
+            return toolkit.h.redirect_to(controller='user',
                                       action='read',
-                                      id=base.c.userobj.name)
+                                      id=toolkit.c.userobj.name)
         else:
-            log.error("No userobj received in Repoze.who Shibboleth controller %r " % base.c)
-            base.h.flash_error(_("No user info received for login"))
-            return base.h.redirect_to('/')
+            log.error("No userobj received in Repoze.who Shibboleth controller %r " % toolkit.c)
+            toolkit.h.flash_error(_("No user info received for login"))
+            return toolkit.h.redirect_to('/')
